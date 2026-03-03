0bbf8f94a2566449ab9413969ec4680889d254fa
aaron blum <blumaa@gmail.com>
2026-03-03 22:39:16 +0100

    fix build

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["whatsapp-web.js"],
-  experimental: {
-    turbo: {
-      watchOptions: {
-        ignored: ["**/.wwebjs_auth/**", "**/.wwebjs_cache/**"],
-      },
-    },
-  },
 };
 
 export default nextConfig;
